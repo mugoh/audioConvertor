ac4c713dbeadd2056a22423dcacfc166eaa8ce74
root <mugoh.ks@gmail.com>
2019-02-17 10:35:26 +0300

    Receive input path command

--- convertor/cli.py
+++ convertor/cli.py
@@ -4,3 +4,18 @@
 """
 
 import click
+
+
+@click.command(context_settings={
+    "ignore_unknown_options": True
+})
+@click.argument('input_files', nargs=-1,
+                type=click.Path(exists=True),
+                help="The path to file(s) to convert")
+def load_files(input_files):
+    for file in input_files:
+        click.echo(file)
+
+
+if __name__ == '__main__':
+    load_files()
