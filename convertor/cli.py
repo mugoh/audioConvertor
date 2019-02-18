0c0aaa8032e20ad9b5646c9186820de270abe3e4
root <mugoh.ks@gmail.com>
2019-02-18 14:49:38 +0300

    feat(interface) Play audio from specified path

--- convertor/cli.py
+++ convertor/cli.py
@@ -8,29 +8,46 @@ import os
 
 
 @click.group()
-@click.pass_context
-@click.option('--verbose', '-v', help="Increase output verbosity level")
-def main(ctx, verbose):
-    """
-        audio3 is a command line tool that helps convert video files
-        to audio file formats.\n
-         example: audio3 convert -i input/file/path -o output/path
-    """
-    pass
-
-
-@main.command('convert')
 @click.option('--input_directory', '-i', nargs=1, type=click.Path(exists=True),
               required=True, help="Directory to get files to convert")
 @click.option('--output', '-o', nargs=1, type=click.Path(exists=True),
               help="Path to save converted file.\n" +
               "Defaults to the current working directory if not specified",
               default='.')
+@click.option('--bitrate', '-b', type=int,
+              help="Audio bitrate specification in kbps e.g 192")
+@click.option('--verbose', '-v', help="Increase output verbosity level")
+@click.pass_context
+def main(ctx, input_directory, output, bitrate, verbose):
+    """
+        audio3 is a command line tool that helps convert video files
+        to audio file formats.\n
+         example: audio3 convert -i input/file/path -o output/path
+    """
+    ctx.obj = input_directory
+    ctx.obj = output
+
+
+"""@main.command('convert')
+@click.option('--input_directory', '-i', nargs=1, type=click.Path(exists=True),
+              required=True, help="Directory to get files to convert")
+@click.option('--output', '-o', nargs=1, type=click.Path(exists=True),
+              help="Path to save converted file.\n" +
+              "Defaults to the current working directory if not specified",
+              default='.')
+@click.option('--bitrate', '-b', type=int,
+              help="Audio bitrate specification in kbps e.g 192")
 @click.pass_obj
-def load_files(ctx, input_directory, output):
+"""
+
+
+@main.command('convert')
+@click.pass_obj
+def load_files(ctx):
     """
         :   Convert video file input to audio.
     """
+    output = ctx.obj
     for file in os.listdir(output):
         click.echo(file)
 
@@ -41,7 +58,7 @@ def load_files(ctx, input_directory, output):
 @click.pass_obj
 def load_audio(ctx, playlist):
     """
-        Selects a track of audio files and loads them up
+        :   Selects a track of audio files and loads them up
         in a music player.
     """
     playlist = os.listdir(playlist)
