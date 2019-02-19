423be73afd421f2da3fde780f019d86d46642b33
root <mugoh.ks@gmail.com>
2019-02-19 19:49:00 +0300

    Add convertions handler class

---
+++ convertor/convertor.py
@@ -0,0 +1,12 @@
+"""
+    This module holds the class that makes
+    subprocess calls to ffmeg with the received
+    CLI commands.
+"""
+
+
+class Convertor:
+    """
+        Makes calls to subprocesses with arguments
+        and commands received from the CLI.
+    """
