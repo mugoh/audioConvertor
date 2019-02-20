69d26fcbd19b80b598e36762372a0ab48d4940a4
root <mugoh.ks@gmail.com>
2019-02-20 19:02:16 +0300

    Check existence of ffmpeg on start

--- convertor/convertor.py
+++ convertor/convertor.py
@@ -3,6 +3,8 @@
     subprocess calls to ffmeg with the received
     CLI commands.
 """
+import subprocess
+import shutil
 
 
 class Convertor:
@@ -10,3 +12,15 @@ class Convertor:
         Makes calls to subprocesses with arguments
         and commands received from the CLI.
     """
+    def __init__():
+        """
+            Checks for presence of ffmpeg on program start
+            in urs/bin and prompts for installation.
+        """
+
+        if not shutil.which('ffmpeg'):
+            install_prompt = input(
+                "Need to get ffmpeg for you. Continue?[y/N] ")
+            accepted_res = ['y',  'n']
+            while install_prompt.lower()[0] not in accepted_res:
+                install_prompt = input()
