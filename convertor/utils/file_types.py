b2d642ec1a3978848df8255f96d9b5e7ac79402d
root <mugoh.ks@gmail.com>
2019-02-22 19:30:44 +0300

    Return filee check as boolean value

--- convertor/utils/file_types.py
+++ convertor/utils/file_types.py
@@ -1,8 +1,12 @@
-import click
+"""
+    This module holds functions that help in
+    type checking and of the application arguments
+    and function parameters.
+"""
 
 
 def check_is_video(file_name):
     formats = ['flv', 'mp4', 'avi']
 
-    return [extension for extension in
-            formats if file_name.endswith(extension)]
+    return any([extension for extension in
+                formats if file_name.endswith(extension)])
