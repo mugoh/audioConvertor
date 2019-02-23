63925359831e353d6908600b0140d6c95a8dd5b1
root <mugoh.ks@gmail.com>
2019-02-23 14:03:18 +0300

    Clear click commands in utils

--- convertor/utils/file_types.py
+++ convertor/utils/file_types.py
@@ -4,6 +4,7 @@
     and function parameters.
 """
 import click
+import subprocess
 
 
 def check_is_video(file_name):
@@ -17,12 +18,32 @@ def check_is_video(file_name):
                 formats if file_name.endswith(extension)])
 
 
-@click.argument('--choice_', choices=['y', 'n'],
-                prompt="Need to get ffmpeg. Continue? [y/N]")
-def require_ffmepg(choice_):
+@click.command()
+@click.option('--choice_', type=click.Choice(['y', 'n'],
+                                             case_sensitive=False))
+def require_ffmepg():
     """
         Prompts for installation of ffmpeg from user
         if missing. If user specifies 'No', the
         convertion is terminated.
     """
+    choices = ['y', 'n']
+
+    choice_ = input("Need to get ffmpeg. Continue?[y/n] ").lower().strip()[0]
     click.echo("choice " + choice_)
+
+    while choice_ not in choices:
+        choice_ = input("Continue? y/n ").lower().strip()[0]
+    if choice_ == 'n':
+        return
+    elif choice_ == 'y':
+        get_module('ffmpeg')
+
+
+def get_module(module):
+    """
+        Fetches and installs dependency that matches
+        given parameter name.
+    """
+
+    subprocess.Popen(['pip', 'install', module])
