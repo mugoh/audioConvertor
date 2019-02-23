ae0d1c093272db8d34a6749fb71ca696b60cec8c
root <mugoh.ks@gmail.com>
2019-02-23 13:31:18 +0300

    Prompt installation of ffmpeg

--- convertor/utils/file_types.py
+++ convertor/utils/file_types.py
@@ -3,6 +3,7 @@
     type checking and of the application arguments
     and function parameters.
 """
+import click
 
 
 def check_is_video(file_name):
@@ -10,3 +11,9 @@ def check_is_video(file_name):
 
     return any([extension for extension in
                 formats if file_name.endswith(extension)])
+
+
+@click.argument('--choice', choices=['y', 'n'],
+                prompt="Need to get ffmpeg. Continue? [y/N]")
+def require_ffmepg(choice):
+    click.echo("choice " + choice)
