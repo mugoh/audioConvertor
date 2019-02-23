0584224fdae3046cb2cc6fbdf5225b0c656a93eb
root <mugoh.ks@gmail.com>
2019-02-23 13:36:48 +0300

    Give importation module docs

--- convertor/utils/file_types.py
+++ convertor/utils/file_types.py
@@ -7,13 +7,22 @@ import click
 
 
 def check_is_video(file_name):
+    """
+        Ensures passed file inputs are of recogninsed
+        media format.
+    """
     formats = ['flv', 'mp4', 'avi']
 
     return any([extension for extension in
                 formats if file_name.endswith(extension)])
 
 
-@click.argument('--choice', choices=['y', 'n'],
+@click.argument('--choice_', choices=['y', 'n'],
                 prompt="Need to get ffmpeg. Continue? [y/N]")
-def require_ffmepg(choice):
-    click.echo("choice " + choice)
+def require_ffmepg(choice_):
+    """
+        Prompts for installation of ffmpeg from user
+        if missing. If user specifies 'No', the
+        convertion is terminated.
+    """
+    click.echo("choice " + choice_)
