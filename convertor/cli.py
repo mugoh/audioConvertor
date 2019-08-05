9135a13d1bed6f0920da3b7325b387fb9433d849
mugoh <mugoh.ks@gmail.com>
2019-08-05 15:30:00 +0300

    fix option passing in play

--- convertor/cli.py
+++ convertor/cli.py
@@ -134,6 +134,7 @@ def load_audio(ctx, playlist, recursive, player):
         click.echo(click.style('Specify the file playlist location', fg='red'))
         return
 
+    playlist = playlist[0] if not isinstance(playlist, str) else playlist
     if recursive:
         try:
 
@@ -152,6 +153,7 @@ def load_audio(ctx, playlist, recursive, player):
             return
 
     if not convertor_instance.is_video(playlist):
+        playlist = playlist[0] if not isinstance(playlist, str) else playlist
         click.echo(click.style(
             playlist + " is not a supported media type", fg='red'))
         return
@@ -176,6 +178,3 @@ def flatten(iterable):
 
 
 convertor_instance = Convertor()
-
-if __name__ == '__main__':
-    main()
